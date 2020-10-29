bf0ad88b01e72dfe628f99ccf176545913584360
Laura Stevens <lmrstevens3@gmail.com>
2020-10-29 05:21:40 -0600

    Added FAQ to Nav Bar and changed Learn to Tutorials until PMP Learn page is deleted. Added quick links to user guide for common topics.

--- js/toggle_functions.js
+++ js/toggle_functions.js
@@ -10,7 +10,6 @@ function toggle_content(id, leave_open) {
     }
 }
 
-
 function toggle_sub(id, icon_id, leave_open, sub_id,  sub_icon_id) {
     toggle_content(id, leave_open);
     toggle(sub_id, sub_icon_id);
@@ -34,40 +33,4 @@ function toggle(id, icon_id, leave_open) {
     }
 }
 
-// function toggle_caret(id, caret_id) {
-//     toggle_content(id);
-//     const caret = document.getElementById(caret_id);
-//     caret.classList.toggle("fa-caret-down");
-//     caret.classList.toggle("fa-caret-right");
-// }
-//
-// function toggle_plus(id, plus_id) {
-//     toggle_content(id);
-//     const icon = document.getElementById(plus_id);
-//     icon.classList.toggle("fa-plus");
-//     icon.classList.toggle("fa-minus");
-// }
-//
-// function toggle_angle(id, angle_id) {
-//     toggle_content(id);
-//     const icon = document.getElementById(angle_id);
-//     icon.classList.toggle("fa-angle-down");
-//     icon.classList.toggle("fa-angle-right");
-// }
-
-
-// function collapse(id, icon, leave_open) {
-//     const content = document.getElementById(id);
-//     if (content.style.display !== "block" || leave_open) {
-//         content.style.display = "block";
-//     } else {
-//         content.style.display = "none";
-//
-//     }
-// }
-
-// function collapse_sub(id, leave_open, sub_id) {
-//     collapse(id, leave_open);
-//     collapse(sub_id);
-// }
 
